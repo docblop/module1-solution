2f35ba829c8370a99b20c21a6df8e9f460d71404
Mike Schaefer <docblop@gmail.com>
2018-04-25 11:45:34 -0400

    first module1 posting

--- app.js
+++ app.js
@@ -9,19 +9,24 @@
     DIController.$inject = ['$scope', '$filter'];
     function DIController($scope, $filter) {
 
+        $scope.splitsies = "";
         $scope.counter = 0;
 
         $scope.Check = function () {
-            var checks = $scope.splitsies.split(",");
             $scope.counter = 0;
-            checks.forEach(function (check) {
-                if (check.trim().length > 0) {
-                    $scope.counter++;
-                }
-            });
+            if ($scope.splitsies != "") {
+                var checks = $scope.splitsies.split(",");
+                checks.forEach(function (check) {
+                    if (check.trim().length > 0) {
+                        $scope.counter++;
+                    }
+                });
+            }
 
-            if ($scope.counter >= 4) {
-                $scope.message = "Too much!";
+            if ($scope.splitsies == '') {
+                $scope.message = "Please enter data first!";
+            } else if ($scope.counter >= 4) {
+                    $scope.message = "Too much!";
             } else if ($scope.counter==0) {
                 $scope.message = "Please enter data first!";
             } else {
